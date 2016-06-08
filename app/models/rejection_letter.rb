3ec0e3d7f15149af14b135c5c7b476ccfdb525c2
Kevin Chandler <im.kevin@me.com>
2016-06-07 20:44:05 -0700

    model validations

--- app/models/rejection_letter.rb
+++ app/models/rejection_letter.rb
@@ -1,2 +1,7 @@
 class RejectionLetter < ActiveRecord::Base
+  # require either a rejection letter message or image
+  validates_presence_of :text, if: lambda { |rl| rl.image_url.nil? }
+  validates_presence_of :image_url, if: lambda { |rl| rl.text.nil? }
+
+  validates :notes, length: { maximum: 1000 }
 end
